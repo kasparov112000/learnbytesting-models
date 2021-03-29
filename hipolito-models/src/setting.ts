1f3ac2dc2ac46472e55d323069d09df0c9e80fec
Hipolito Nunez <212marines@gmail.com>
2021-03-29 09:25:33 -0400

    add hideFooter property to setting.ts

--- hipolito-models/src/setting.ts
+++ hipolito-models/src/setting.ts
@@ -2,12 +2,14 @@ export class Setting {
     public _id?: string;
     public createAvailable: boolean = true;
     public allowDownloadWordDocumentEnabled: boolean = false;
+    public hideFooter: boolean = false;
 
-    constructor(init?: { _id?: string, createAvailable?: boolean, allowDownloadWordDocumentEnabled?: boolean }) {
+    constructor(init?: { _id?: string, hideFooter?: boolean, createAvailable?: boolean, allowDownloadWordDocumentEnabled?: boolean }) {
         if(init) {
             this._id = init._id;
             this.createAvailable = init.createAvailable;
             this.allowDownloadWordDocumentEnabled = init.allowDownloadWordDocumentEnabled;
+            this.hideFooter = init.hideFooter;
         }
     }
 }
