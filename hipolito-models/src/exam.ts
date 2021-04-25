3c36971175e8794b5edb7bc3ffd292fb51c60bc0
Hipolito Nunez <212marines@gmail.com>
2021-04-25 07:28:54 -0400

    change exam class

--- hipolito-models/src/exam.ts
+++ hipolito-models/src/exam.ts
@@ -1,7 +1,7 @@
 import { FileMetaData } from './file-meta-data';
 import { Category } from './category';
 import { LookupItem } from './lookup-item';
-import { Question } from './question';
+import { QuestionInfo } from './question-info';
 
 export class Exam {
     public _id!: string;
@@ -12,7 +12,7 @@ export class Exam {
     public createdDate!: Date;
     public modifiedDate!: Date;
     public version!: string;
-    public examQuestions: Array<Question> = new Array<Question>();
+    public examQuestions: Array<QuestionInfo> = new Array<QuestionInfo>();
     public examFiles: Array<FileMetaData> = new Array<FileMetaData>();
     public createUuid!: string;
     public active!: boolean;
